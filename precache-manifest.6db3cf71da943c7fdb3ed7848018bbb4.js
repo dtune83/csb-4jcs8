70ebc1147c2a870cd6af5c0d6aa464870870800c
Code Sandbox <hello@codesandbox.io>
2021-10-31 18:25:19 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.6db3cf71da943c7fdb3ed7848018bbb4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3a04e6be822e4b9b177e536973c18e9",
+    "url": "https://dtune83.github.io/csb-4jcs8/index.html"
+  },
+  {
+    "revision": "19bb30606e2f0369b1d2",
+    "url": "https://dtune83.github.io/csb-4jcs8/static/css/main.38a36c10.chunk.css"
+  },
+  {
+    "revision": "f368f6d03d91f738da31",
+    "url": "https://dtune83.github.io/csb-4jcs8/static/js/2.279f532a.chunk.js"
+  },
+  {
+    "revision": "19bb30606e2f0369b1d2",
+    "url": "https://dtune83.github.io/csb-4jcs8/static/js/main.beeedad9.chunk.js"
+  },
+  {
+    "revision": "cdfa4a0ef47f5f73be17",
+    "url": "https://dtune83.github.io/csb-4jcs8/static/js/runtime~main.da991037.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "https://dtune83.github.io/csb-4jcs8/precache-manifest.6db3cf71da943c7fdb3ed7848018bbb4.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://dtune83.github.io/csb-4jcs8/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
